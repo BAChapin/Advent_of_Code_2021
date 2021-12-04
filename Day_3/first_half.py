aed8ad5d5d561aa456ffd27b17adf744cb538082
Brett Chapin <brett.chapin89@gmail.com>
2021-12-03 23:44:01 -0500

    Cleanup for Day 3.

--- Day_3/first_half.py
+++ Day_3/first_half.py
@@ -1,28 +1,28 @@
 
+def process(lines, at_index):
+    threshold = len(lines) / 2
+    most_common_pos = 0
+
+    for line in lines:
+        pos_int = int(line[at_index])
+        most_common_pos += pos_int
+    else:
+        if most_common_pos >= threshold:
+            return "1", "0"
+        else:
+            return "0", "1"
+
 if __name__ == "__main__":
     with open("input.txt", "r") as file:
         lines = file.readlines()
         most_common = ""
         least_common = ""
-        input_len = len(lines)
-        line_len = len(lines[0])
 
-        for position in range(0, line_len - 1):
-            most_common_pos = 0
-            for line in lines:
-                pos_int = int(line[position])
-                most_common_pos += pos_int
-            else:
-                if most_common_pos >= (input_len / 2):
-                    most_common += "1"
-                    least_common += "0"
-                else:
-                    most_common += "0"
-                    least_common += "1"
+        for position in range(0, len(lines[0]) - 1):
+            most_common_pos, least_common_pos = process(lines, position)
+            most_common += most_common_pos
+            least_common += least_common_pos
         else:
-            print("Raw Gamma Rate - ", most_common)
-            print("Raw Epsilon Rate - ", least_common)
-
             decimal_gamma = int(most_common, 2)
             decimal_epsilon = int(least_common, 2)
             print("Answer: ", decimal_epsilon * decimal_gamma)
--- Day_3/second_half.py
+++ Day_3/second_half.py
@@ -14,39 +14,29 @@ def split_lists(at_index, given_list, return_most):
         else:
             ones_list.append(item)
     else:
-        if most_common >= (input_len / 2):
-            if return_most:
-                return ones_list
-            else:
-                return zeros_list
-        else:
-            if return_most:
-                return zeros_list
-            else:
-                return ones_list
+        if most_common >= (input_len / 2) and return_most:
+            return ones_list
+        elif most_common < (input_len / 2) and return_most:
+            return zeros_list
+        elif most_common >= (input_len / 2) and not return_most:
+            return zeros_list
+        elif most_common < (input_len / 2) and not return_most:
+            return ones_list
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as file:
         lines = file.readlines()
-        running_oxygen = lines
-        running_co2 = lines
-        oxygen_rating = ""
-        co2_rating = ""
+        oxygen_readings = lines
+        co2_readings = lines
 
         for index in range(0, len(lines[0]) - 1):
-            if len(running_co2) == 1:
-                co2_rating = running_co2[0]
-            else:
-                running_co2 = split_lists(index, running_co2, False)
+            if len(oxygen_readings) > 1:
+                oxygen_readings = split_lists(index, oxygen_readings, True)
 
-            if len(running_oxygen) == 1:
-                oxygen_rating = running_oxygen[0]
-            else:
-                running_oxygen = split_lists(index, running_oxygen, True)
+            if len(co2_readings) > 1:
+                co2_readings = split_lists(index, co2_readings, False)
         else:
-            print("Raw Oxegen: ", oxygen_rating)
-            print("Raw CO2: ", co2_rating)
-            oxygen = int(oxygen_rating, 2)
-            co2 = int(co2_rating, 2)
+            oxygen = int(oxygen_readings[0], 2)
+            co2 = int(co2_readings[0], 2)
             print("Life Support Rating: ", oxygen * co2)
