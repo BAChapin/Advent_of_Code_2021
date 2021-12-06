8206cd4a6056934e1f8699f95fb00df47a2d8801
Brett Chapin <brett.chapin89@gmail.com>
2021-12-06 00:51:51 -0500

    Added a function to time the execution of other functions
    * Note: Since I hadn't optimized first_half.py in today's puzzle, you can see the substantial time difference between the first_half and second_half. I may update the first_half to use the same form of logic as the second_half, but we'll see.

--- helper.py
+++ helper.py
@@ -1,6 +1,13 @@
+from time import time
 
 def get_lines():
     file = open("input.txt", "r")
     lines = file.readlines()
     file.close()
     return lines
+
+def time_func(func):
+    start_time = time()
+    func()
+    end_time = time()
+    print(end_time - start_time)
