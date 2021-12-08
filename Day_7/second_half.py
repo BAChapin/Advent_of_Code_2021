775eac0e7e3e3c5962f716a4cd45f3ec137be0eb
Brett Chapin <brett.chapin89@gmail.com>
2021-12-08 11:34:54 -0500

    fixup! Update the README.md and completed the puzzles for Day 7.

--- Day_7/second_half.py
+++ Day_7/second_half.py
@@ -7,21 +7,20 @@ def calculate_fuel_consumption(distance):
 
     for travel in range(1, distance + 1):
         consumed += travel
-    
-    return consumed
+    else:
+        return consumed
 
-def fuel_costs_to_position(position, crabs):
+def fuel_costs_to_position(average, crabs):
     total_consumed = 0
 
     for position in crabs:
-        total_consumed += calculate_fuel_consumption(abs(position - position))
+        total_consumed += calculate_fuel_consumption(abs(average - position))
 
     return total_consumed
 
 def process():
     lines = get_lines()
     crab_positions = [int(num) for num in lines[0].split(",")]
-    unique_positions = set(crab_positions)
     average = int(sum(crab_positions) / len(crab_positions))
     consumed_fuel = fuel_costs_to_position(average, crab_positions)
 
