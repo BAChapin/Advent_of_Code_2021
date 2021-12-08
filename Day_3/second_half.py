bc56c62b35c5f2ff9d94a96bc5484970084495fb
Brett Chapin <brett.chapin89@gmail.com>
2021-12-08 12:23:08 -0500

    Implemented get_lines helper function for Day 3

--- Day_3/second_half.py
+++ Day_3/second_half.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("..")
-from helper import time_func
+from helper import get_lines, time_func
 
 def split_lists(at_index, given_list, return_most):
     input_len = len(given_list)
@@ -27,21 +27,20 @@ def split_lists(at_index, given_list, return_most):
             return ones_list
 
 def process():
-    with open("input.txt", "r") as file:
-        lines = file.readlines()
-        oxygen_readings = lines
-        co2_readings = lines
+    lines = get_lines()
+    oxygen_readings = lines
+    co2_readings = lines
 
-        for index in range(0, len(lines[0]) - 1):
-            if len(oxygen_readings) > 1:
-                oxygen_readings = split_lists(index, oxygen_readings, True)
+    for index in range(0, len(lines[0]) - 1):
+        if len(oxygen_readings) > 1:
+            oxygen_readings = split_lists(index, oxygen_readings, True)
 
-            if len(co2_readings) > 1:
-                co2_readings = split_lists(index, co2_readings, False)
-        else:
-            oxygen = int(oxygen_readings[0], 2)
-            co2 = int(co2_readings[0], 2)
-            print("Life Support Rating: ", oxygen * co2)
+        if len(co2_readings) > 1:
+            co2_readings = split_lists(index, co2_readings, False)
+    else:
+        oxygen = int(oxygen_readings[0], 2)
+        co2 = int(co2_readings[0], 2)
+        print("Life Support Rating: ", oxygen * co2)
 
 if __name__ == "__main__":
     time_func(process)
