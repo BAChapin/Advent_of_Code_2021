7a100b0303f21b219e4133b8a2bd5efd190b1c31
Brett Chapin <brett.chapin89@gmail.com>
2021-12-08 11:45:10 -0500

    Implemented function timing for Day 3

--- Day_3/second_half.py
+++ Day_3/second_half.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("..")
+from helper import time_func
 
 def split_lists(at_index, given_list, return_most):
     input_len = len(given_list)
@@ -23,8 +26,7 @@ def split_lists(at_index, given_list, return_most):
         elif most_common < (input_len / 2) and not return_most:
             return ones_list
 
-
-if __name__ == "__main__":
+def process():
     with open("input.txt", "r") as file:
         lines = file.readlines()
         oxygen_readings = lines
@@ -40,3 +42,6 @@ if __name__ == "__main__":
             oxygen = int(oxygen_readings[0], 2)
             co2 = int(co2_readings[0], 2)
             print("Life Support Rating: ", oxygen * co2)
+
+if __name__ == "__main__":
+    time_func(process)
